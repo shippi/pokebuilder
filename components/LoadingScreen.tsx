936497fcc7dea2ee2ed66ea41535d36eb5e8bc78
David <hn94@students.waikato.ac.nz>
2024-05-14 22:33:46 +1200

    spinner added for loading screen

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function LoadingScreen() {
-  return (
-    <div className="h-screen w-screen fixed top-0 overflow-hidden bg-gray-100 z-50">
+  const [counter, setCounter] = useState(1);
+  const periods = ".".repeat(counter % 4);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(counter + 1);
+    }, 200);
 
+    return () => {
+      clearInterval(interval);
+    };
+  });
+  return (
+    <div className="flex justify-center items-center h-screen w-screen fixed top-0 overflow-hidden bg-gray-100 z-50">
+    <div
+      className="inline-block h-12 w-12 animate-spin rounded-full border-8 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite]"
+      role="status">
+      <span
+        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+        >Loading...
+      </span>
+      </div>
     </div>
   )
 }
