c796826e1a521961c6a9569844a191b4c99d35f3
David <hn94@students.waikato.ac.nz>
2024-05-18 00:41:23 +1200

    hover effects added and colours for light mode

--- app/pokedex/page.tsx
+++ app/pokedex/page.tsx
@@ -58,40 +58,40 @@ function Pokedex() {
 
   return (
     <main className="flex flex-col items-center w-[95vw] h-screen max-w-4xl min-w-[324px]">
-        <header className="flex flex-wrap w-full justify-between items-center h-18 gap-x-6 gap-y-2 p-4 rounded-t-xl bg-stone-300 dark:bg-stone-900">
-            <h1 className="font-black text-3xl text-stone-700 dark:text-white">
-                Pokédex
-            </h1>
+      <header className="flex flex-wrap w-full justify-between items-center h-18 gap-x-6 gap-y-2 p-4 rounded-t-xl bg-stone-300 dark:bg-stone-900">
+				<h1 className="font-black text-3xl text-stone-700 dark:text-white">
+						Pokédex
+				</h1>
             
-			<div className="flex flex-wrap  items-center gap-x-6 gap-y-3">
-				<Dropdown 
-					className="w-[100px]"
-					selectedClassName="justify-between px-2 hover:bg-transparent hover:text-indigo-700 dark:hover:bg-transparent dark:hover:text-indigo-300 "
-					selected={[currentGenText]} 
-					listItems = {[<button onClick={() => setCurrentGen(0)} className="w-full px-4 py-2 text-left">All Gens</button>, ...
-						Array(GEN_COUNTS.length - 1)
-							.fill(<li></li>)
-							.map((item, i) => (
-								<button key={i} className="w-full px-4 py-2 text-left" onClick={() => setCurrentGen(i + 1)}>
-									Gen {i+1}
-								</button>
-					))]} 
-				/>
-				<div className="h-[24px] w-[1px] bg-neutral-500 "/>
-				<SearchBar setSearchFn={setSearch}/>
-			</div>
-        </header>
-				<section className={`flex flex-wrap overflow-scroll justify-around w-full gap-x-4 gap-y-12 px-8 py-8 bg-neutral-200 dark:bg-stone-800 ${isLoading && "justify-center items-center"}`} style={{maxHeight: sectionHeight}} ref={listRef}>
-					{
-						isLoading ? 
-						<Spinner/>
-						:
-						data?.
-							slice(currentGenStart, currentGenEnd)
-							.filter(item => item.name.includes(search.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()) || search.replace(/\D/g,'') && item.url.split("/").slice(-2, -1)[0].includes(search.replace(/\D/g,'')))
-							.map(item => <PokemonCard key={item.name} pokemonData={item}/>)
-					}			
-				</section>
+				<div className="flex flex-wrap  items-center gap-x-6 gap-y-3">
+					<Dropdown 
+						className="w-[100px]"
+						selectedClassName="justify-between px-2 hover:bg-transparent hover:text-indigo-700 dark:hover:bg-transparent dark:hover:text-indigo-300 "
+						selected={[currentGenText]} 
+						listItems = {[<button onClick={() => setCurrentGen(0)} className="w-full px-4 py-2 text-left">All Gens</button>, ...
+							Array(GEN_COUNTS.length - 1)
+								.fill(<li></li>)
+								.map((item, i) => (
+									<button key={i} className="w-full px-4 py-2 text-left" onClick={() => setCurrentGen(i + 1)}>
+										Gen {i+1}
+									</button>
+						))]} 
+					/>
+					<div className="h-[24px] w-[1px] bg-neutral-500 "/>
+					<SearchBar setSearchFn={setSearch}/>
+				</div>
+      </header>
+			<section className={`flex flex-wrap overflow-scroll justify-around w-full gap-x-4 gap-y-12 px-8 py-8 bg-neutral-200 dark:bg-stone-800 ${isLoading && "justify-center items-center"}`} style={{maxHeight: sectionHeight}} ref={listRef}>
+				{
+					isLoading ? 
+					<Spinner/>
+					:
+					data?.
+						slice(currentGenStart, currentGenEnd)
+						.filter(item => item.name.includes(search.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()) || search.replace(/\D/g,'') && item.url.split("/").slice(-2, -1)[0].includes(search.replace(/\D/g,'')))
+						.map(item => <PokemonCard key={item.name} pokemonData={item}/>)
+				}			
+			</section>
     </main>
   )
 }
--- components/UI/HamburgerMenu.tsx
+++ components/UI/HamburgerMenu.tsx
@@ -7,18 +7,21 @@ interface Props {
 function HamburgerMenu({ closeFn } : Props) {
   return (
 		<div className="absolute h-screen w-screen backdrop-brightness-50 backdrop-blur-xl z-[100] duration-0">
-			<ul className="absolute w-[256px] h-96 left-0 rounded-xl m-6 p-6 pt-3 font-bold text-stone-100 bg-stone-800 animate-menuSlide">
+			<ul className="absolute w-[256px] h-96 left-0 rounded-xl m-6 p-6 pt-3 font-bold text-stone-800 dark:text-stone-100 bg-stone-200 dark:bg-stone-800 animate-menuSlide">
 				<li className="flex items-center justify-between py-3">
-					<Link href="/" onClick={() => closeFn()}>Home</Link>
-					<button className="flex items-center h-[24px]" onClick={() => closeFn()}>
+					<Link href="/" className="hover:text-stone-500 dark:hover:text-stone-400" onClick={() => closeFn()}>Home</Link>
+					<button 
+						className="flex items-center h-[24px] hover:text-red-700 dark:hover:text-red-400" 
+						onClick={() => closeFn()}
+					>
 						<i className="bi bi-x text-2xl"/>
 					</button>
 				</li>
 				<li className="py-3">
-					<Link href="/pokedex" onClick={() => closeFn()}>Pokedex</Link>
+					<Link href="/pokedex" className="hover:text-stone-500 dark:hover:text-stone-400" onClick={() => closeFn()}>Pokedex</Link>
 				</li>
 				<li className="py-3">
-					<Link href="/planner" onClick={() => closeFn()}>Team Builder</Link>
+					<Link href="/planner" className="hover:text-stone-500 dark:hover:text-stone-400" onClick={() => closeFn()}>Team Builder</Link>
 				</li>
 			</ul>
 		</div>
