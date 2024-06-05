9987a6ba353135b24d650b61b844454fa165b1b7
David <hn94@students.waikato.ac.nz>
2024-06-05 20:42:26 +1200

    b

--- components/TeamPlanner/TypeCoverage.tsx
+++ components/TeamPlanner/TypeCoverage.tsx
@@ -1,11 +1,13 @@
 'use client'
 
 import { TeamPlannerContext } from "@/context/TeamPlannerContext"
+import { TYPE_CHART } from "@/helpers/consts";
 import { useContext, useEffect } from "react"
 
 export default function TypeCoverage() {
   const { selectedMoves } = useContext(TeamPlannerContext);
-  console.log(selectedMoves)
+  console.log(TYPE_CHART.normal)
+  
   return (
     <div className="flex flex-col justify-center w-full">
       <h1 className="text-2xl font-bold">Team Type Coverage</h1>
